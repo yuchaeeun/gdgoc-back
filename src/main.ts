cc09d042a04898b65cc25a9c8c78a308bc1ce663
yuchaeeun <fromcodeblue@gmail.com>
2025-11-01 17:44:32 +0900

    first commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      ‘{https://port-next-gdgoc-front-mhg09kkvc32b102e.sel3.cloudtype.app/}',
+    ],
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT ?? 5000);
+}
+bootstrap();
